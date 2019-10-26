5bed063bdf88d7071c810e404c8098bbe0f6e841
iganari <cigaguri@gmail.com>
2019-10-27 05:06:24 +0900

    format

--- storage/terraform/variables.tf
+++ storage/terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "common" {
-    type = "map"
-    default = {
-        rsg_name = "rsg-sample"
-        rsg_location = "West Europe"
-    }
+  type = "map"
+  default = {
+    rsg_name     = "rsg-sample"
+    rsg_location = "West Europe"
+  }
 }
