a5c8c0edf8924d0310ca08ce6b70c7006afbc2f1
iganari <cigaguri@gmail.com>
2019-11-03 08:31:04 +0900

    change name of upload file

--- storage/terraform-sample-management-policy/storage.tf
+++ storage/terraform-sample-management-policy/storage.tf
@@ -38,17 +38,19 @@ resource "azurerm_storage_container" "asc_default" {
 #   source                 = "images/irasutoya/penguin/animal_chara_smartphone_penguin.png"
 # }
 
-resource "azurerm_storage_blob" "asb_default_03" {
+resource "azurerm_storage_blob" "asb_hiyoko" {
   count = "${length(var.image-hiyoko)}"
 
   # same version
-  name = "sample03/hiyoko/${element(var.image-hiyoko, count.index)}"
-  # # rename version
-  # name = "sample03/hiyoco-${lookup(var.image-hiyoko, count.index)}"
+  name = "sample/hiyoko-${element(var.image-hiyoko, count.index)}"
 
   # resource_group_name    = "${azurerm_resource_group.arg_default.name}"
   storage_account_name   = "${azurerm_storage_account.asa_default.name}"
   storage_container_name = "${azurerm_storage_container.asc_default.name}"
   type                   = "Block"
   source                 = "images/irasutoya/hiyoko/${element(var.image-hiyoko, count.index)}"
+}
+
+output "check_asb_default" {
+  value = "${azurerm_storage_blob.asb_hiyoko}"
 }
