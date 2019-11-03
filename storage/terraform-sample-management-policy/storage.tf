9421c4c7f4e068d0d62b8f8d71aa2bb1052c87ee
iganari <cigaguri@gmail.com>
2019-11-03 14:00:41 +0900

    add managed policy(lifecycle management)

--- storage/terraform-sample-management-policy/storage.tf
+++ storage/terraform-sample-management-policy/storage.tf
@@ -12,6 +12,7 @@ resource "azurerm_storage_account" "asa_default" {
   location                 = "${lookup(var.common, "rsg_location")}"
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  account_kind             = "BlobStorage"
 }
 
 resource "azurerm_storage_container" "asc_default" {
@@ -20,23 +21,6 @@ resource "azurerm_storage_container" "asc_default" {
   storage_account_name  = "${azurerm_storage_account.asa_default.name}"
   container_access_type = "private"
 }
-
-# resource "azurerm_storage_blob" "asb_default_01" {
-#   name                   = "penguin_01.png"
-#   # resource_group_name    = "${azurerm_resource_group.arg_default.name}"
-#   storage_account_name   = "${azurerm_storage_account.asa_default.name}"
-#   storage_container_name = "${azurerm_storage_container.asc_default.name}"
-#   type                   = "Block"
-#   source                 = "images/irasutoya/penguin/animal_chara_computer_penguin.png"
-# }
-# resource "azurerm_storage_blob" "asb_default_02" {
-#   name                   = "sample/penguin_02.png"
-#   resource_group_name    = "${azurerm_resource_group.arg_default.name}"
-#   storage_account_name   = "${azurerm_storage_account.asa_default.name}"
-#   storage_container_name = "${azurerm_storage_container.asc_default.name}"
-#   type                   = "Block"
-#   source                 = "images/irasutoya/penguin/animal_chara_smartphone_penguin.png"
-# }
 
 resource "azurerm_storage_blob" "asb_hiyoko" {
   count = "${length(var.image-hiyoko)}"
@@ -50,10 +34,10 @@ resource "azurerm_storage_blob" "asb_hiyoko" {
   type                   = "Block"
   source                 = "images/irasutoya/hiyoko/${element(var.image-hiyoko, count.index)}"
 }
-
-# output "check_asb_default" {
-#   value = "${azurerm_storage_blob.asb_hiyoko}"
-# }
+# 
+# # output "check_asb_default" {
+# #   value = "${azurerm_storage_blob.asb_hiyoko}"
+# # }
 
 
 
@@ -66,4 +50,27 @@ resource "azurerm_storage_blob" "asb_penguin" {
   count  = "${length(var.image-penguin)}"
   name   = "sample/penguin-${element(var.image-penguin, count.index)}"
   source = "images/irasutoya/penguin/${element(var.image-penguin, count.index)}"
+}
+
+resource "azurerm_storage_management_policy" "policy_penguin" {
+  storage_account_id = "${azurerm_storage_account.asa_default.id}"
+
+  rule {
+    name = "policy2penguin"   ## A rule name can contain any combination of alpha numeric characters.
+    enabled = true
+    filters {
+      prefix_match = ["sample/penguin-"]
+      blob_types = ["blockBlob"]
+    }
+    actions {
+      base_blob {
+        tier_to_cool_after_days_since_modification_greater_than = 1
+        tier_to_archive_after_days_since_modification_greater_than = 2
+        delete_after_days_since_modification_greater_than = 3
+      }
+      snapshot {
+        delete_after_days_since_creation_greater_than = 30
+      }
+    }
+  }
 }
