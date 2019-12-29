a435394f4c30fb8232e02a8038a312ced1e2a4af
iganari <cigaguri@gmail.com>
2019-12-29 10:56:56 +0900

    update main terraform setting

--- aks/terraform/main.tf
+++ aks/terraform/main.tf
@@ -35,4 +35,46 @@ resource "azurerm_log_analytics_solution" "alas_aks" {
   }
 }
 
-resource "azurerm_kubernetes_cluster"
+resource "azurerm_kubernetes_cluster" "akc_aks" {
+  name = var.cluster_name
+  location = azurerm_resource_group.arg_aks.location
+  resource_group_name = azurerm_resource_group.arg_aks.name
+  dns_prefix = var.dns_prefix
+
+  linux_profile {
+    admin_username = "ubuntu"
+
+    ssh_key {
+      key_data = file(var.ssh_public_key)
+    }
+  }
+
+  default_node_pool {
+    name = "agentpool"
+    couont = var.agent_count
+    vm_size = "Standard_DS1_v2"
+    os_type = "Linux"
+    os_disk_size_gb = 30
+  }
+
+  service_principal {
+    oms_agent {
+      enable = true
+      log_analytics_workspace_id = azurerm_log_analytics_workspace.alaw_aks.id
+    }
+  }
+
+  tags = {
+    Environment = "Development"
+  }
+
+
+
+
+
+
+
+
+
+
+}
--- aks/terraform/variables.tf
+++ aks/terraform/variables.tf
@@ -6,3 +6,15 @@ variable "resource_group_name" {
 variable "log_analytics_workspace_sku" {
   default = "PerGB2018"
 }
+
+variable "cluster_name" {
+  defautl = "iganari-aks-test"
+}
+
+variable "dns_prefix" {
+  default = "WIP"
+}
+
+variable "agent_count" {
+  default = "WIP"
+}
