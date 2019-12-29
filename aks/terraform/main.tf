4d9855ca47c09b4a628b79e829f319cf02cc1896
iganari <cigaguri@gmail.com>
2019-12-30 00:22:15 +0900

    update Terraform setting

--- aks/terraform/main.tf
+++ aks/terraform/main.tf
@@ -1,68 +1,75 @@
 terraform {
-  version = "0.12.18"
+  required_version = "0.12.18"
 }
 # provider "azurerm"{
 #   version = ""
 # }
 
-resource "azurerm_resource_group" "arg_aks"{
+resource "azurerm_resource_group" "aks-test" {
   name     = var.resource_group_name
-  location = var.location 
+  location = var.location
 }
 
 resource "random_id" "rnd_suffix" {
   byte_length = 8
 }
 
-resource "azurerm_log_analytics_workspace" "alaw_aks" {
-  name = "hoge"
-  location = "hoge"
-  resource_group_name = azurerm_resource_group.arg_aks
-  sku = var.log_analytics_workspace_sku
+resource "azurerm_log_analytics_workspace" "aks-test" {
+  name                = "hoge"
+  location            = "hoge"
+  resource_group_name = azurerm_resource_group.aks-test
+  sku                 = var.log_analytics_workspace_sku
 
 }
 
-resource "azurerm_log_analytics_solution" "alas_aks" {
-  solution_name        = "ContainerInsights"
-  location             = azurerm_log_analytics_workspace.alaw_aks.location
-  resource_group_name  = azurerm_resource_group.arg_aks.name
-  workspace_resource_id = azurerm_log_analytics_workspace.alaw_aks.id
-  workspace_name = azurerm_log_analytics_workspace.alaw_aks.name
+resource "azurerm_log_analytics_solution" "aks-test" {
+  solution_name         = "ContainerInsights"
+  location              = azurerm_log_analytics_workspace.aks-test.location
+  resource_group_name   = azurerm_resource_group.aks-test.name
+  workspace_resource_id = azurerm_log_analytics_workspace.aks-test.id
+  workspace_name        = azurerm_log_analytics_workspace.aks-test.name
 
   plan {
     publisher = "Microsoft"
-    product = "OMGallery/ContainerInsights"
+    product   = "OMGallery/ContainerInsights"
   }
 }
 
-resource "azurerm_kubernetes_cluster" "akc_aks" {
-  name = var.cluster_name
-  location = azurerm_resource_group.arg_aks.location
-  resource_group_name = azurerm_resource_group.arg_aks.name
-  dns_prefix = var.dns_prefix
+resource "azurerm_kubernetes_cluster" "aks-test" {
+  name                = var.cluster_name
+  location            = azurerm_resource_group.aks-test.location
+  resource_group_name = azurerm_resource_group.aks-test.name
+  dns_prefix          = var.dns_prefix
 
   linux_profile {
     admin_username = "ubuntu"
 
-    ssh_key {
-      key_data = file(var.ssh_public_key)
-    }
+    # ssh_key {
+    #   key_data = file(var.ssh_public_key)
+    # }
   }
 
   default_node_pool {
-    name = "agentpool"
-    couont = var.agent_count
-    vm_size = "Standard_DS1_v2"
-    os_type = "Linux"
+    name            = "agentpool"
+    couont          = var.agent_count
+    vm_size         = "Standard_DS1_v2"
+    os_type         = "Linux"
     os_disk_size_gb = 30
   }
 
   service_principal {
+    client_id     = var.client_id
+    client_secret = var.client_secret
+  }
+
+  addon_profile {
     oms_agent {
-      enable = true
-      log_analytics_workspace_id = azurerm_log_analytics_workspace.alaw_aks.id
+      enable                     = true
+      log_analytics_workspace_id = azurerm_log_analytics_workspace.aks-test.id
     }
   }
+
+
 
   tags = {
     Environment = "Development"
--- aks/terraform/variables.tf
+++ aks/terraform/variables.tf
@@ -2,13 +2,27 @@ variable "resource_group_name" {
   default = "iganari-aks-test"
 }
 
+variable "location" {
+  default = "Central US"
+}
+
+
+
+variable "client_id" {
+  default = ""
+}
+
+variable "client_secret" {
+  defautl = ""
+}
+
 # refer https://azure.microsoft.com/pricing/details/monitor/ for log analytics pricing 
 variable "log_analytics_workspace_sku" {
   default = "PerGB2018"
 }
 
 variable "cluster_name" {
-  defautl = "iganari-aks-test"
+  default = "iganari-aks-test"
 }
 
 variable "dns_prefix" {
