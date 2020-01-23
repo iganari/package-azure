20d11562c759bbd20ea335e86fb24a57090aa147
iganari <cigaguri@gmail.com>
2020-01-23 17:11:24 +0900

    update path

--- aks/terraform/prepare/docker-build-run.sh
+++ aks/terraform/prepare/docker-build-run.sh
@@ -4,7 +4,7 @@ set -xeu
 
 _I_TAG='k8s-sample'
 BASEPATH=$(cd `dirname $0`; pwd)
-DIREPATH=`echo $BASEPATH | awk -F\/ '{print $NF}'`
+DIREPATH=`echo $BASEPATH/../ | awk -F\/ '{print $NF}'`
 
 set +eu
 docker rm -f ${_I_TAG}
@@ -14,7 +14,7 @@ docker build . -t ${_I_TAG}
 
 docker run --rm \
            -it \
-           -v $BASEPATH:/opt/hejda/$DIREPATH \
+           -v $BASEPATH/../:/opt/hejda/$DIREPATH \
            -w /opt/hejda/$DIREPATH \
            -h ${_I_TAG} \
            --name ${_I_TAG} \
