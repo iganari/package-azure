c6148e69bc1f47bb939c42953d46ad39783c8ba9
iganari / Toru Igarashi <cigaguri@gmail.com>
2020-04-06 00:12:56 +0900

    Update resource_group.tf

--- virtual-machines/linux-terraform/resource_group.tf
+++ virtual-machines/linux-terraform/resource_group.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "pkg-azure" {
+resource "azurerm_resource_group" "pkg-azure-vm-linux" {
   name = "package-azure-sample"
   location = "West Europe" # Todo: tokyo
 }
